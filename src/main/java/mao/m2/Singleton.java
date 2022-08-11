3fd97c23596a51afddb9aab1b9ad36ad2726bbba
mao <1296193245@qq.com>
2022-08-11 21:35:54 +0800

    java_design_patterns_singleton_pattern

--- src/main/java/mao/m2/Singleton.java
+++ src/main/java/mao/m2/Singleton.java
@@ -1,16 +1,16 @@
-package mao.m1;
+package mao.m2;
 
 /**
  * Project name(项目名称)：java设计模式_单例模式
- * Package(包名): mao.m1
+ * Package(包名): mao.m2
  * Class(类名): Singleton
  * Author(作者）: mao
  * Author QQ：1296193245
  * GitHub：https://github.com/maomao124/
  * Date(创建日期)： 2022/8/11
- * Time(创建时间)： 20:56
+ * Time(创建时间)： 21:08
  * Version(版本): 1.0
- * Description(描述)： 饿汉式-方式1（静态变量方式）
+ * Description(描述)： 饿汉式-方式2（静态代码块方式）
  */
 
 public class Singleton
@@ -30,7 +30,14 @@ public class Singleton
         System.out.println("实例私有化构造方法");
     }
 
-    private static final Singleton instance = new Singleton();
+    private static final Singleton instance;
+
+
+    static
+    {
+        System.out.println("创建单例对象实例");
+        instance = new Singleton();
+    }
 
     /**
      * 对外提供静态方法获取该对象
---
+++ src/main/java/mao/m3/Singleton.java
@@ -0,0 +1,52 @@
+package mao.m3;
+
+/**
+ * Project name(项目名称)：java设计模式_单例模式
+ * Package(包名): mao.m3
+ * Class(类名): Singleton
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/8/11
+ * Time(创建时间)： 21:14
+ * Version(版本): 1.0
+ * Description(描述)： 懒汉式-方式1（线程不安全）
+ */
+
+public class Singleton
+{
+    public String str = "hello world";
+
+    public String show()
+    {
+        return "show";
+    }
+
+    /**
+     * 私有化构造方法
+     */
+    private Singleton()
+    {
+        System.out.println("类加载完成");
+    }
+
+    private static Singleton instance;
+
+    /**
+     * 对外提供方法获取该对象
+     * 线程不安全
+     *
+     * @return Singleton对象
+     */
+    public static Singleton getInstance()
+    {
+        if (instance == null)
+        {
+            //线程1运行到这里，进行上下文切换，切换到线程2，线程2进行判空，并进行赋值，
+            //赋值后，线程2切换到线程1，由于判空已经执行，直接创建对象，对象创建两次
+            System.out.println("创建对象实例");
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
