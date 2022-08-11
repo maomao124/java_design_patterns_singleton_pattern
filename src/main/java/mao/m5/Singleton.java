4263978fde1be07f37720de21fb3d83f686be672
mao <1296193245@qq.com>
2022-08-11 22:44:26 +0800

    java_design_patterns_singleton_pattern

---
+++ src/main/java/mao/m5/Singleton.java
@@ -0,0 +1,61 @@
+package mao.m5;
+
+/**
+ * Project name(项目名称)：java设计模式_单例模式
+ * Package(包名): mao.m5
+ * Class(类名): Singleton
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/8/11
+ * Time(创建时间)： 22:28
+ * Version(版本): 1.0
+ * Description(描述)： 懒汉式-方式3（双重检查锁）
+ * <p>
+ * 在多线程的情况下，可能会出现空指针问题，出现问题的原因是JVM在实例化对象的时候会进行优化和指令重排序操作。
+ * 要解决双重检查锁模式带来空指针异常的问题，只需要使用 `volatile` 关键字, `volatile` 关键字可以保证可见性和有序性。
+ */
+
+public class Singleton
+{
+    public String str = "hello world";
+
+    public String show()
+    {
+        return "show";
+    }
+
+    /**
+     * 私有化构造方法
+     */
+    private Singleton()
+    {
+        System.out.println("实例私有化构造方法");
+    }
+
+    private static volatile Singleton instance;
+
+    /**
+     * 对外提供方法获取该对象
+     * 线程安全
+     *
+     * @return Singleton对象
+     */
+    public static Singleton getInstance()
+    {
+        //第一次判断，如果instance不为null，不进入抢锁阶段，直接返回实例
+        if (instance == null)
+        {
+            synchronized (Singleton.class)
+            {
+                //抢到锁之后再次判断是否为null
+                if (instance == null)
+                {
+                    System.out.println("创建对象实例");
+                    instance = new Singleton();
+                }
+            }
+        }
+        return instance;
+    }
+}
